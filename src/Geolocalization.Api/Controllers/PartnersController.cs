b3e16233f3a2b579d9c1b32aef5b692ebf63b1da
Guilherme Sampaio dos Santos <guilherme.sampaio@easynvest.com.br>
2019-10-04 18:47:22 -0300

    Create initial project structure

---
+++ src/Geolocalization.Api/Controllers/PartnersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geolocalization.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PartnersController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(id);
+        }
+    }
+}
