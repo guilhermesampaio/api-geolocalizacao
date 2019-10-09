4c820691f95c74084ff7bcbb1101771eeafb8eb7
guilherme <santos.gsampaio@gmail.com>
2019-10-09 02:25:13 -0300

    Add unit tests

---
+++ test/Geolocation.Tests/Application/Command/CreatePartnerCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using Geolocalization.Application.Command.Commands;
+using Geolocalization.Application.Command.Handlers;
+using Geolocalization.Domain.Entities;
+using Geolocalization.Domain.Repositories;
+using MongoDB.Bson;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Geolocation.Tests.Application.Command
+{
+    public class CreatePartnerCommandHandlerTests
+    {
+        private readonly IPartnersRepository _repository;
+        private readonly CreatePartnerCommandHandler _handler;
+
+        public CreatePartnerCommandHandlerTests()
+        {
+            _repository = Substitute.For<IPartnersRepository>();
+            _handler = new CreatePartnerCommandHandler(_repository);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldCreatePartner()
+        {
+            // Arrange
+            var command = new CreatePartnerCommand()
+            {
+                Address = new Geolocalization.Application.Command.Commands.Point(),
+                CoverageArea = new Geolocalization.Application.Command.Commands.MultiPolygon()
+            };
+            var generatedId = ObjectId.GenerateNewId().ToString();
+            _repository.Create(Arg.Any<Partner>()).Returns(generatedId);
+
+            // Act
+            var response = await _handler.Handle(command, default(CancellationToken));
+
+            // Assert
+            await _repository.Received(1).Create(Arg.Any<Partner>());
+
+        }
+    }
+}
---
+++ test/Geolocation.Tests/Application/Query/GetPartnerByIdQueryHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using Geolocalization.Application.Query.Handlers;
+using Geolocalization.Application.Query.Queries;
+using Geolocalization.Domain.Entities;
+using Geolocalization.Domain.Repositories;
+using MongoDB.Bson;
+using NSubstitute;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Geolocation.Tests.Application.Query
+{
+    public class GetPartnerByIdQueryHandlerTests
+    {
+        private readonly IPartnersRepository _repository;
+        private readonly GetPartnerByIdQueryHandler _handler;
+
+        public GetPartnerByIdQueryHandlerTests()
+        {
+            _repository = Substitute.For<IPartnersRepository>();
+            _handler = new GetPartnerByIdQueryHandler(_repository);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldGetPartner()
+        {
+            // Arrange
+            var id = ObjectId.GenerateNewId().ToString();
+            var command = new GetPartnerByIdQuery(id);
+            
+            _repository.Get(id).Returns(default(Partner));
+
+            // Act
+            var response = await _handler.Handle(command, default(CancellationToken));
+
+            // Assert
+            await _repository.Received(1).Get(id);
+
+        }
+    }
+}
